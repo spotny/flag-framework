3bea4a9222aabf6d03627d141db65c5661b6b5e6
Francisco Costa <jfcpcosta@gmail.com>
2024-03-27 10:37:11 +0000

    feat: add flash bag

---
+++ src/Http/FlashBag.php
@@ -0,0 +1,32 @@
+<?php namespace Flag\Framework\Http;
+
+class FlashBag {
+
+    const NAME = 'flash_bag';
+
+    public static function add(string $message, string $type = 'info'): void {
+        $bag = Request::session(self::NAME);
+
+        if (is_null($bag)) {
+            $bag = [];
+        }
+
+        $bag[] = [
+            'type' => $type,
+            'message' => $message
+        ];
+
+        Request::session(self::NAME, $bag);
+    }
+
+    public static function has(): bool {
+        return Request::session(self::NAME) && count(Request::session(self::NAME)) > 0;
+    }
+
+    public static function get(): array {
+        $bag = Request::session(self::NAME);
+        unset($_SESSION[self::NAME]);
+
+        return $bag;
+    }
+}
